29b4cc1acedee858dec28761e10af778ccc2edff
Alchemix <sts@smac.fritz.box>
2016-04-14 19:16:28 +0200

    Added a single temperature sensor returning a constant value.

--- index.js
+++ index.js
@@ -4,6 +4,8 @@ var storage = require('node-persist');
 var uuid = require('HAP-NodeJS').uuid;
 var Bridge = require('HAP-NodeJS').Bridge;
 var Accessory = require('HAP-NodeJS').Accessory;
+var Service = require('HAP-NodeJS').Service;
+var Characteristic = require('HAP-NodeJS').Characteristic;
 var accessoryLoader = require('HAP-NodeJS/lib/AccessoryLoader');
 
 console.log("HAP-NodeJS starting...");
@@ -13,6 +15,8 @@ storage.initSync();
 
 // Start by creating our Bridge which will host all loaded Accessories
 var bridge = new Bridge('Node Bridge', uuid.generate("Node Bridge"));
+//bridge.username = "C1:5D:3A:AE:5E:FA";
+//bridge.pincode = "031-45-154";
 
 // Listen for bridge identification event
 bridge.on('identify', function(paired, callback) {
@@ -28,6 +32,21 @@ var accessories = accessoryLoader.loadDirectory(dir);
 accessories.forEach(function(accessory) {
   bridge.addBridgedAccessory(accessory);
 });
+
+var ac = new Accessory("Temperatur", uuid.generate('Temp'));
+ac.username = "C1:5D:3A:AE:5E:FB";
+ac.pincode = "031-45-155";
+ac
+  .addService(Service.TemperatureSensor)
+  .getCharacteristic(Characteristic.CurrentTemperature)
+  .on('get', function(callback) {
+    console.log("get");
+    callback(null, 24);
+  });;
+
+
+bridge.addBridgedAccessory(ac);
+
 
 // Publish the Bridge on the local network.
 bridge.publish({
