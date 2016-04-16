41f73f559f41c15de0d78aca44f95e3866a81321
Alchemix <sts@smac.fritz.box>
2016-04-16 12:19:33 +0200

    Temperatur wird aus fixem mqtt-Kanal gelesen

--- index.js
+++ index.js
@@ -8,15 +8,18 @@ var Service = require('HAP-NodeJS').Service;
 var Characteristic = require('HAP-NodeJS').Characteristic;
 var accessoryLoader = require('HAP-NodeJS/lib/AccessoryLoader');
 
+var mqtt    = require('mqtt');
+
+
 console.log("HAP-NodeJS starting...");
 
 // Initialize our storage system
 storage.initSync();
 
 // Start by creating our Bridge which will host all loaded Accessories
-var bridge = new Bridge('Node Bridge', uuid.generate("Node Bridge"));
-//bridge.username = "C1:5D:3A:AE:5E:FA";
-//bridge.pincode = "031-45-154";
+var bridge = new Bridge('Mqtt Bridge', uuid.generate("Mqtt Bridge"));
+bridge.username = "C1:5D:3A:AE:5E:FA";
+bridge.pincode = "031-45-154";
 
 // Listen for bridge identification event
 bridge.on('identify', function(paired, callback) {
@@ -34,15 +37,14 @@ accessories.forEach(function(accessory) {
 });
 
 var ac = new Accessory("Temperatur", uuid.generate('Temp'));
-ac.username = "C1:5D:3A:AE:5E:FB";
-ac.pincode = "031-45-155";
+ac.temp = 13;
 ac
   .addService(Service.TemperatureSensor)
   .getCharacteristic(Characteristic.CurrentTemperature)
   .on('get', function(callback) {
     console.log("get");
-    callback(null, 24);
-  });;
+    callback(null, ac.temp);
+  }.bind(this));;
 
 
 bridge.addBridgedAccessory(ac);
@@ -55,3 +57,18 @@ bridge.publish({
   pincode: "031-45-154",
   category: Accessory.Categories.BRIDGE
 });
+
+var client  = mqtt.connect('mqtt://tau');
+client.on('connect', function () {
+  client.subscribe('/home/esp1/tempC');
+  client.publish('presence', 'Hello mqtt');
+});
+
+client.on('message', function (topic, message) {
+  // message is Buffer
+  console.log(message.toString());
+  ac.temp = message.toString();
+  ac
+    .getService(Service.TemperatureSensor)
+    .setCharacteristic(Characteristic.CurrentTemperature, ac.temp);
+}.bind(this));
